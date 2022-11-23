f3203c2be7cac0633197d507cc1861e7d564c412
Lieke <lieke.vereecke@UGent.be>
2022-11-23 15:44:18 +0100

    Iedereen is er officieel juj

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Lieke");
         System.out.println("BZ is cool");
         System.out.println("Rune");
+        System.out.println("Iedereen is er jujj");
     }
 }
