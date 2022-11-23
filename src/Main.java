bb2d1a8642b65553ece59c81747480943513a23c
Lieke <lieke.vereecke@UGent.be>
2022-11-23 15:38:18 +0100

    Lieke testjee

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Morane");
         System.out.println("Britt");
+        System.out.println("Lieke");
     }
 }
