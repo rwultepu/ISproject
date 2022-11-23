0e6d28d1d47262cb859dc4ea7f4d5723b3e92dc8
EmiliaVyncke <emilia.vyncke@ugent.be>
2022-11-23 15:41:27 +0100

    test Emilia

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Morane");
         System.out.println("Britt");
         System.out.println("Lieke");
+        System.out.println("BZ is cool");
     }
 }
