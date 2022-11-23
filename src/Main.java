2352ef6de62f3195b17b3fdf02fccb4c8acb894f
brittdecraen <britt.decraen@UGent.be>
2022-11-23 15:36:48 +0100

    britt test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Morane");
+        System.out.println("Britt");
     }
-
 }
