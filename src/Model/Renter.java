43f9887aa3932546f441f6d99c125c317419300c
EmiliaVyncke <emilia.vyncke@ugent.be>
2022-12-06 18:36:28 +0100

    renter toegevoegde methodes getTransactionHistoryOfRenter, getTopOwners, getAllRecommendedClothing

--- src/Model/Renter.java
+++ src/Model/Renter.java
@@ -1,28 +1,53 @@
 package Model;
 
+import Database.OwnerDAO;
 import Database.TransactionDAO;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Renter extends User {
+public class  Renter extends User {
 
     TransactionDAO transactionDAO = new TransactionDAO();
+    OwnerDAO ownerDAO = new OwnerDAO();
 
 
     public Renter(String firstName, String lastName, int userID, String email, String phoneNumber, Date userBirth, String streetName, int streetNumber, String city, int zipCode) {
         super(firstName, lastName, userID, email, phoneNumber, userBirth, streetName, streetNumber, city, zipCode);
     }
 
-    public ArrayList<Transaction> getTransactionHistoryOfRenter(User userID){
+    public ArrayList<Transaction> getTransactionHistoryOfRenter(int userID){
+        ArrayList<Transaction> transactionHistoryOfRenter = new ArrayList<>();
+
         TransactionDAO transactionDao= new TransactionDAO();
         ArrayList<Transaction> getTransactionHistoryOfRenter= transactionDao.getAllTransactions();
-        for()
-            if()
-                getTransactionHistoryOfRenter.add();
 
+        for(Transaction transaction : getTransactionHistoryOfRenter)
+            if(userID == transaction.getUserID())
+                transactionHistoryOfRenter.add(transaction);
+
+        return transactionHistoryOfRenter;
     }
+
+
     public ArrayList<Owner> getTopOwners(){
+        ArrayList<Owner> topOwners = new ArrayList<>();
+        for(Owner o: ownerDAO.getAllOwners())
+            if(o.getAverageProductReviewScore>=3 && o.getAverageServiceReviewScore>=3)
+                topOwners.add(o);
+        return topOwners;
+    }
+
+    public ArrayList<ArrayList<Clothing>> getAllRecommendedClothing(int userID){
+        ArrayList<ArrayList<Clothing>> recommendedClothing=new ArrayList<>();
+        for(Owner o: getTopOwners())
+            if(getTopOwners().contains(o))
+                recommendedClothing.add(o.getAllClothesOfOwner());
+        return recommendedClothing;
+    }
+
+    public ArrayList<Clothing> getClothingWithSpecificParameters(){
 
     }
 }
