9e6be9b950b7da8ba1758041129d1b33625f7451
EmiliaVyncke <emilia.vyncke@ugent.be>
2022-12-06 16:31:32 +0100

    Renter klasse Emilia

--- src/Model/Renter.java
+++ src/Model/Renter.java
@@ -1,17 +1,29 @@
 package Model;
 
+import Database.TransactionDAO;
+
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 
 public class Renter extends User {
 
-    //private HashMap<String, String> purchaseClothings;
-    //private HashMap<String, String> historyRentedClothings;
+    TransactionDAO transactionDAO = new TransactionDAO();
 
 
     public Renter(String firstName, String lastName, int userID, String email, String phoneNumber, Date userBirth, String streetName, int streetNumber, String city, int zipCode) {
         super(firstName, lastName, userID, email, phoneNumber, userBirth, streetName, streetNumber, city, zipCode);
+    }
+
+    public ArrayList<Transaction> getTransactionHistoryOfRenter(User userID){
+        TransactionDAO transactionDao= new TransactionDAO();
+        ArrayList<Transaction> getTransactionHistoryOfRenter= transactionDao.getAllTransactions();
+        for()
+            if()
+                getTransactionHistoryOfRenter.add();
+
+    }
+    public ArrayList<Owner> getTopOwners(){
+
     }
 }
 
