20203d3cb3d7e3e05be49b3d22ae7a4cdddd7c5f
Lieke <lieke.vereecke@UGent.be>
2022-12-06 17:21:56 +0100

    Owner klasse Lieke tot nu toe

--- src/Model/Owner.java
+++ src/Model/Owner.java
@@ -5,10 +5,13 @@ import Database.OwnerDAO;
 import Database.TransactionDAO;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
 public class Owner extends User {
+
+    Owner owner;
 
     private String causeName;
     private double selectedPercentageToCauseOfOwner;
@@ -35,17 +38,45 @@ public class Owner extends User {
         return selectedPercentageToCauseOfOwner;
     }
 
+
+    public ArrayList<Clothing> getAllClothesOfOwner(){
+        ArrayList<Clothing> clothingsOfOwner = new ArrayList<>();
+        for(Clothing clothings1Per1: clothingDAO.getAllClothings()){
+            if(clothings1Per1.getUserID() == owner.getUserID()){
+                clothingsOfOwner.add(clothings1Per1);
+            }
+        }
+    }
+
     public double getTotalRevenueOfTheMonth(){
         double totalRenvenueOfTheMonth = 0.0;
         for(Transaction transactions1Per1: transactionDAO.getAllTransactions()){
-            for(Clothing clothings1Per1: clothingDAO.getAllClothings()){
-                if(clothings1Per1.getClothingID() == transactions1Per1.getClothingID()){
-                    ownerDAO.getOwner(clothings1Per1.getUserID());
-                }
-            }
+            if(transactions1Per1.getDateOfTransaction().)
+                //Klopt mijn meegegeven parameter?
+                // Hoe moet ik kijken of de maand van de dateOfTransaction gelijk is aan de meegegeven month
+                Calendar.getInstance().getTime().getMonth();
+            //waaerom welken deze methodes niet?
         }
         return totalRenvenueOfTheMonth;
     }
+
+    public void updatePercentageToCauseOfOwner(double newPercentage){
+        if(newPercentage>0){
+            selectedPercentageToCauseOfOwner = newPercentage;
+        }
+        else {
+            System.out.println("Het te doneren percentage moet groter zijn dan 0%!");
+        }
+    }
+
+    public ArrayList<Transaction> getTransactionHistoryOfOwner(){
+        ArrayList<Transaction> transactionHistoryOfOwner = new ArrayList<>();
+        for(Transaction transactions1Per1: transactionDAO.getAllTransactions()){
+            for(int clothingIDsOfOwner1Per1: owner.getAllClothesOfOwner().)
+        }
+    }
+
+
 
 }
 
