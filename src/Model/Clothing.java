dbccfe421b40f5275e5cb0986b61284d89d81395
moranedelanghe <morane.delanghe@ugent.be>
2022-12-06 15:19:12 +0100

    Clothing klasse pushen met nieuwe instantievariabelen, methode getTransactionOfClothing en halve methode isAvailable

--- src/Model/Clothing.java
+++ src/Model/Clothing.java
@@ -1,17 +1,22 @@
 package Model;
 
+import Database.TransactionDAO;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Clothing {
 
     private String clothingID;
     private double price;
+    private int userID;
     private String categoryName;
     private String description;
 
-    public Clothing(String clothingID, double price, String categoryName, String description){
+    public Clothing(String clothingID, double price, int userID, String categoryName, String description){
         this.clothingID = clothingID;
         this.price = price;
+        this.userID = userID;
         this.categoryName = categoryName;
         this.description = description;
     }
@@ -31,7 +36,32 @@ public class Clothing {
         return description;
     }
 
-    //Deze methode retourneert aan welk gender het kledingstuk gelinkt is.
+    public int getUserID() {
+        return userID;
+    }
+
+    TransactionDAO transactionDAO = new TransactionDAO();
+
+    //Methode die Arraylist maakt van alle transacties van een bepaald kledingstuk instaat:
+    public ArrayList<Transaction> getTransactionsOfClothing(int clothingID){
+       ArrayList<Transaction> transactionsOfCLothing = new ArrayList<>();
+
+       for (Transaction t : transactionDAO.getAllTransactions())
+           if(clothingID == t.getClothingID())
+               transactionsOfCLothing.add(t);
+       return transactionsOfCLothing;
+    }
+
+    //Methode die checkt of kledij nog beschikbaar is tussen meegegeven startdate en enddate:
+    public boolean isAvailable(Date wantedStartDate, Date wantedEndDate, int userID){
+        for()
+    }
+
+
+
+
+
+        //Deze methode retourneert aan welk gender het kledingstuk gelinkt is.
     /*
     public String getGender(){
         String gender = "male";
