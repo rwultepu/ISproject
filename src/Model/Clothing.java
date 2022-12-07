c68b3a2198c2857c739b72c5c7b2aeb34df35e8e
moranedelanghe <morane.delanghe@ugent.be>
2022-12-07 14:35:03 +0100

    Clothing klasse af

--- src/Model/Clothing.java
+++ src/Model/Clothing.java
@@ -1,6 +1,8 @@
 package Model;
 
 import Database.ClothingDAO;
+import Database.OwnerDAO;
+import Database.RenterDAO;
 import Database.TransactionDAO;
 
 import java.time.LocalDate;
@@ -16,6 +18,8 @@ public class Clothing {
     private int userID;
     private String categoryName;
     private String description;
+    // counter die ClothingID bepaalt, telt op als een nieuw clothing object wordt weggeschreven
+    private int counter = 1;
     TransactionDAO transactionDAO = new TransactionDAO();
     ClothingDAO clothingDAO = new ClothingDAO();
 
@@ -53,11 +57,70 @@ public class Clothing {
     public boolean isAvailable(LocalDate wantedStartDate, LocalDate wantedEndDate, int clothingID){
         boolean flag = true;
         for(Transaction t : getTransactionsOfClothing(clothingID))
-            if (!wantedStartDate.isAfter(t.getEndDateLD()) || !wantedEndDate.isBefore(t.getStartDateLD()) {
+            if (!wantedStartDate.isAfter(stringToLocalDate(t.getEndDate())) || !wantedEndDate.isBefore(stringToLocalDate(t.getStartDate()))){
                 flag = false;
                 break;
             }
         return flag;
+    }
+
+    public boolean isValidDate(String date){
+        boolean flag = false;
+
+        int dayString = Integer.parseInt(date.substring(0,1));
+        int monthString = Integer.parseInt(date.substring(3,4));
+        int yearString = Integer.parseInt(date.substring(6,9));
+
+        boolean leapYear = (yearString%4 == 0);
+
+
+        if(leapYear){
+            if((yearString%100 == 0) && (yearString%400 !=0))
+                leapYear = false;}
+
+        if((monthString>= 1) && (monthString<= 12))
+            if ((monthString == 9)||(monthString == 4)||(monthString == 11 )||(monthString == 6) ) {
+                if ((dayString >= 1) && (dayString <= 30))
+                    flag = true;
+                else
+                    flag = false;
+            }
+
+            else if ((monthString == 2)) {
+                if ((leapYear))
+                    if ((dayString <= 29) && (dayString >= 1))
+                        flag = true;
+                    else
+                        flag = false;
+
+                else if ((dayString <= 28) && (dayString >= 1))
+                    flag = true;
+                else
+                    flag = false;
+            }
+
+            else
+            if ((dayString>=1)&& (dayString<=31))
+                flag = true;
+            else
+                flag = false;
+
+        else
+            flag = false;
+
+        return flag;
+    }
+    public LocalDate stringToLocalDate(String date){
+        LocalDate stringToLocalDate = null;
+
+        int day = Integer.parseInt(date.substring(0, 1));
+        int month = Integer.parseInt(date.substring(3, 4));
+        int year = Integer.parseInt(date.substring(6,9));
+
+        if(isValidDate(date))
+            stringToLocalDate = LocalDate.of(day, month, year);
+
+        return stringToLocalDate;
     }
 
     // Methode retourneert een arraylist met alle kledij die beschikbaar is tussen de gegeven datums:
@@ -69,23 +132,11 @@ public class Clothing {
         return availableClothing;
     }
 
-     */
-
-    //Deze methode retourneert aan welk gender het kledingstuk gelinkt is.
-    /*
-    public String getGender(){
-        String gender = "male";
-        String genderRetreval = getCategory().getCategoryName().substring(0,0);
-        if(genderRetreval.equals(Category.Gender.F))
-            return gender = "female";
-
-        return gender;
+    public void addClothing(Clothing clothingInput){
+        clothingDAO.saveClothing(clothingInput);
+        counter++;
     }
-
-    public ArrayList<Transaction> getPreviousTransactions();
-        ArrayList<Transaction> previousTransactions;
-
-        return previousTransactions;
-
-     */
+    public void deleteClothing(Clothing clothingInput){
+        clothingDAO.deleteClothing(clothingInput);
+    }
 }
