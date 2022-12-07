3eb2003df6f14f665d0f22e945851384cc6a52f6
moranedelanghe <morane.delanghe@ugent.be>
2022-12-07 13:49:33 +0100

    Clothing: methodes IsAvailable en getAvailableClothing

--- src/Model/Clothing.java
+++ src/Model/Clothing.java
@@ -1,19 +1,23 @@
 package Model;
 
+import Database.ClothingDAO;
 import Database.TransactionDAO;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 public class Clothing {
 
     private int clothingID;
     private double price;
-
+    //userID van Owner:
     private int userID;
     private String categoryName;
     private String description;
     TransactionDAO transactionDAO = new TransactionDAO();
+    ClothingDAO clothingDAO = new ClothingDAO();
 
     public Clothing(int clothingID, double price, int userID, String categoryName, String description){
         this.clothingID = clothingID;
@@ -46,15 +50,26 @@ public class Clothing {
     }
 
     //Methode die checkt of kledij nog beschikbaar is tussen meegegeven startdate en enddate:
-    public boolean isAvailable(Date wantedStartDate, Date wantedEndDate, int clothingID){
+    public boolean isAvailable(LocalDate wantedStartDate, LocalDate wantedEndDate, int clothingID){
         boolean flag = true;
         for(Transaction t : getTransactionsOfClothing(clothingID))
-            if (!wantedStartDate.after(t.getEndDate()) || !wantedEndDate.before(t.getStartDate())) {
+            if (!wantedStartDate.isAfter(t.getEndDateLD()) || !wantedEndDate.isBefore(t.getStartDateLD()) {
                 flag = false;
                 break;
             }
         return flag;
     }
+
+    // Methode retourneert een arraylist met alle kledij die beschikbaar is tussen de gegeven datums:
+    public ArrayList<Clothing> getAvailableClothing(LocalDate wantedStartDate, LocalDate wantedEndDate){
+        ArrayList <Clothing> availableClothing = new ArrayList<>();
+        for(Clothing c : clothingDAO.getAllClothings())
+            if(c.isAvailable(wantedStartDate, wantedEndDate,c.getClothingID()))
+                availableClothing.add(c);
+        return availableClothing;
+    }
+
+     */
 
     //Deze methode retourneert aan welk gender het kledingstuk gelinkt is.
     /*
