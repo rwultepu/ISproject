76cf950135681c0c3f9059665ba09dd31b6c5de6
EmiliaVyncke <emilia.vyncke@ugent.be>
2022-12-07 15:56:53 +0100

    iserID-> userID

--- src/Database/RenterDAO.java
+++ src/Database/RenterDAO.java
@@ -53,7 +53,7 @@ public class RenterDAO {
 
             String sqlSelect = "SELECT userID "
                     + "FROM renter "
-                    + "WHERE iserID = ? ";
+                    + "WHERE userID = ? ";
 
             PreparedStatement stmt = con.prepareStatement(sqlSelect);
             stmt.setInt(1, renter.getUserID());
